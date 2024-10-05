8c64f14222dff42ea3f9d50b9d2d524aead3d94c
Diogo Oliveira <dluks82@gmail.com>
2024-10-05 16:48:52 -0300

    :wrench: chore: Config `application-test.properties` and `H2`

--- src/main/java/dev/dluks/jsp02/DsJsp02Application.java
+++ src/main/java/dev/dluks/jsp02/DsJsp02Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DsJsp02Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DsJsp02Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DsJsp02Application.class, args);
+    }
 
 }
