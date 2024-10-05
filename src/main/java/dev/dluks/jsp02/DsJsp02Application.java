014899d352270a8fa2ddd79774725af3fd553737
Diogo Oliveira <dluks82@gmail.com>
2024-10-05 14:46:53 -0300

    :tada: chore: Initial commit

---
+++ src/main/java/dev/dluks/jsp02/DsJsp02Application.java
@@ -0,0 +1,13 @@
+package dev.dluks.jsp02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsJsp02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsJsp02Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dluks/jsp02/DsJsp02ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dluks.jsp02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsJsp02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
